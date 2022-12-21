94e7baea9bc20ba2a44611eb3c8b0889ddd4fbee
Hsieh Robin <robin0623.hsieh@gmail.com>
2022-12-21 18:56:14 +0800

    the bug is solved

--- first_set.cpp
+++ first_set.cpp
@@ -1,34 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
-#define DEBUG
+//#define DEBUG
 #define INPUT_NUM 20
 #define PRODUCT_MAX_SIZE 10
 using namespace std;
 
-
+// self-defined class declaration --------------------------------
 typedef struct {
     char symbol;
     bool visitFirst;
+    bool derivesEmpty;
 } Non_Terminal;
 
+// variable definition -------------------------------------------
 string inputArray[INPUT_NUM];
 string input;
 string productArray[PRODUCT_MAX_SIZE][PRODUCT_MAX_SIZE];
+
 vector<Non_Terminal> nonTerminalList;
-bool symbolDerivesEmpty;
+
+//bool symbolDerivesEmpty;
 bool addLambdaInFirstSet;
 
+// function declaration, aka prototype ---------------------------
 vector<string> split(const string &str, const char &delimiter);
 void saveToProductArray(string inputArray_local[], int numbersOfGrammar);
 bool visitedFirst(char thisNonTerminal);
+void changeSymbolDerivesEmpty(char thisNonTerminal, bool status);
+bool symbolDerivesEmpty(char thisNonTerminal);
 void initialVisit();
-vector<char> first(string &alpha);
-void internalFirst(string &alpha, vector<char> &result, size_t Xn, size_t xn);
+vector<char> first(char lhs, string &alpha);
+void internalFirst(char lhs, string &alpha, vector<char> &result, size_t Xn, size_t xn);
 
 
 int main() {
-
     // standard input
     int j = 0;
     while(cin >> input) {
@@ -42,7 +48,6 @@ int main() {
     j /= 2;
 
     saveToProductArray(inputArray, j);
-
 
 
 #if defined(DEBUG)
@@ -68,10 +73,15 @@ int main() {
     cout << "------------------------------" << endl;
 #endif
 
-    // create the non-terminal list, and initial them as being visited first
+    // create the non-terminal list
     for (size_t p_row = 0; p_row < PRODUCT_MAX_SIZE; p_row++) {
         if (!productArray[p_row][0].empty()) {
-            Non_Terminal non_terminal = {productArray[p_row][0].at(0), true};
+            Non_Terminal non_terminal = {};
+            non_terminal.symbol = productArray[p_row][0].at(0);
+            // initial them as being visited first
+            non_terminal.visitFirst = true;
+            // initial them as won't derive empty
+            non_terminal.derivesEmpty = false;
             nonTerminalList.push_back(non_terminal);
         }
     }
@@ -92,19 +102,21 @@ int main() {
             vector<char> firstSet;
 
             initialVisit();
-            addLambdaInFirstSet = false;
 
-            // productArray[p_row][0] is LHS in a grammar
-            // productArray[p_row][1] is the first choose RHS in a grammar
+            /* productArray[p_row][0] is LHS in a grammar
+             * productArray[p_row][1] is the first choose RHS in a grammar */
             int p_column = 1;
+
             // while break when finish an iteration of a set of choices of RHS in a grammar
             while (!productArray[p_row][p_column].empty()) {
-                vector<char> temp = first(productArray[p_row][p_column]);
+                vector<char> temp = first(productArray[p_row][0].at(0) ,productArray[p_row][p_column]);
                 firstSet.insert(firstSet.end(), temp.begin(), temp.end());
                 p_column++;
             }
-            if (addLambdaInFirstSet) {
-                cout << ';';
+
+            // check the LHS of grammar derive empty or not
+            if (symbolDerivesEmpty(productArray[p_row][0].at(0))) {
+                firstSet.push_back(';');
             }
 
             sort(firstSet.begin(), firstSet.end());
@@ -131,6 +143,7 @@ vector<string> split(const string &str, const char &delimiter) {
     }
     return result;
 }
+
 
 void saveToProductArray(string inputArray_local[], int numbersOfGrammar) {
     // save LHS from inputArray to productArray
@@ -171,6 +184,7 @@ void saveToProductArray(string inputArray_local[], int numbersOfGrammar) {
     }
 }
 
+
 bool visitedFirst(char thisNonTerminal) {
     for (size_t i = 0; i < nonTerminalList.size(); ++i) {
         if (nonTerminalList[i].symbol == thisNonTerminal) {
@@ -186,40 +200,64 @@ bool visitedFirst(char thisNonTerminal) {
     return false;
 }
 
+
+void changeSymbolDerivesEmpty(char thisNonTerminal, bool status) {
+    for (size_t i = 0; i < nonTerminalList.size(); ++i) {
+        if (nonTerminalList[i].symbol == thisNonTerminal) {
+            nonTerminalList[i].derivesEmpty = status;
+            break;
+        }
+    }
+}
+
+
+bool symbolDerivesEmpty(char thisNonTerminal) {
+    if (thisNonTerminal == ';') {
+        return true;
+    }
+    for (size_t i = 0; i < nonTerminalList.size(); ++i) {
+        if (nonTerminalList[i].symbol == thisNonTerminal) {
+            return nonTerminalList[i].derivesEmpty;
+        }
+    }
+    return false;
+}
+
+
 void initialVisit() {
     for (size_t i = 0; i < nonTerminalList.size(); i++) {
-
         nonTerminalList[i].visitFirst = true;
     }
 }
-vector<char> first(string &alpha) {
+
+
+vector<char> first(char lhs, string &alpha) {
     vector<char> result;
 
-
+    // how many X do it have in RHS in a grammar Y -> X0 X1 X2 ... Xn
     size_t Xn = alpha.length();
 
-    internalFirst(alpha, result, Xn, 0);
+    // check the first set of first(alpha), the X numbers is Xn, now we are checking X0 first
+    internalFirst(lhs, alpha, result, Xn, 0);
     return result;
 }
 
-void internalFirst(string &alpha, vector<char> &result, size_t Xn, size_t xn) { //Xn is the numbers for RHS: X1 X2 X3 ... Xn, xn is the count
+
+void internalFirst(char lhs, string &alpha, vector<char> &result, size_t Xn, size_t xn) { //Xn is the numbers of RHS: X0 X1 X2 ... Xn, xn is the count
 
     if (xn < Xn) {
 
         // RHS meets lambda, which is represented by ';'
         if (alpha[xn] == ';') {
 
-            addLambdaInFirstSet = true;
-
-            symbolDerivesEmpty = true;
+            // rule 1 for judge whether Y of grammar: Y -> X0 X1 ... Xn will derives empty
+            changeSymbolDerivesEmpty(lhs, true);
             return;
         }
         // RHS meets terminal symbol
         else if ((97 <= alpha[xn] && alpha[xn] <= 122) || alpha[xn] == 36) {
 
             result.push_back(alpha[xn]);
-
-            addLambdaInFirstSet = false;
             return;
         }
         // RHS meets non-terminal symbol
@@ -229,14 +267,15 @@ void internalFirst(string &alpha, vector<char> &result, size_t Xn, size_t xn) { 
 
                 for (size_t p_row = 0; p_row < PRODUCT_MAX_SIZE; p_row++) {
 
+                    // find the grammar in productArray that LHS == alpha[xn]
                     if (!productArray[p_row][0].empty()) {
                         if (productArray[p_row][0].at(0) == alpha[xn]) {
 
                             size_t p_column = 1;
                             while (!productArray[p_row][p_column].empty()) {
-                                symbolDerivesEmpty = false;
+                                //symbolDerivesEmpty = false;
 
-                                internalFirst(productArray[p_row][p_column], result, productArray[p_row][p_column].length(), 0);
+                                internalFirst(productArray[p_row][0].at(0), productArray[p_row][p_column], result, productArray[p_row][p_column].length(), 0);
                                 p_column++;
                             }
                             break;
@@ -244,11 +283,21 @@ void internalFirst(string &alpha, vector<char> &result, size_t Xn, size_t xn) { 
                     }
                 }
             }
-
-            if (symbolDerivesEmpty) {
-
-                internalFirst(alpha, result, Xn, xn + 1);
+            if (symbolDerivesEmpty(alpha[xn])) {
+                internalFirst(lhs, alpha, result, Xn, xn + 1);
             }
+        }
+    }
+    // rule 2 for judge whether Y of grammar: Y -> X0 X1 ... Xn will derives empty
+    else if (xn == Xn) {
+        addLambdaInFirstSet = true;
+        for (size_t i = 0; i < alpha.length(); i++) {
+            if (!symbolDerivesEmpty(alpha[i])) {
+                addLambdaInFirstSet = false;
+            }
+        }
+        if (addLambdaInFirstSet) {
+            changeSymbolDerivesEmpty(lhs, true);
         }
     }
 }
